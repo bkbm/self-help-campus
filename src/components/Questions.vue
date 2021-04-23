26cbc501e6c679f90ba036e4ab26dc7cac3dc2ad
bb <baileykbm66@gmail.com>
2021-04-23 13:05:19 +0100

    Created basic questions ui in its own component

---
+++ src/components/Questions.vue
@@ -0,0 +1,31 @@
+<template>
+  <form>
+      <div class="list" v-for="item in array" :key='item'>
+          <div class="question">
+              <label> {{item}} </label>
+              <br>
+              <input type="textarea">
+          </div>
+      </div>
+      <button>Submit</button>
+  </form>
+</template>
+
+<script>
+export default {
+data() {
+    return{
+        array: ['question 1', 'question 2']
+    }
+},
+methods: {
+    call() {
+        return this.$data.array
+    }
+}
+}
+</script>
+
+<style>
+
+</style>
--- src/views/Worksheets.vue
+++ src/views/Worksheets.vue
@@ -1,10 +1,15 @@
 <template>
   <h2>Worksheets</h2>
+  <Questions />
 </template>
 
 <script>
+import Questions from '../components/Questions.vue'
 export default {
-name: "Worksheets"
+name: "Worksheets",
+components: {
+  Questions
+}
 }
 </script>
 
