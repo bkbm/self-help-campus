67f240015c1c5800ca057ab771690e7ebfc82e01
Emmanuils Borovikovs <eb18847@essex.ac.uk>
2021-04-24 17:17:40 +0100

    Added tailwind.

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,22 +3,13 @@ module.exports = {
     env: {
         node: true,
     },
-    extends: [
-        "plugin:vue/vue3-essential",
-        "eslint:recommended",
-        "@vue/prettier",
-    ],
+    extends: ["plugin:vue/vue3-essential", "eslint:recommended"],
     parserOptions: {
         parser: "babel-eslint",
     },
-    "prettier/prettier": [
-        "error",
-        {
-            endOfLine: "auto",
-        },
-    ],
     rules: {
         "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
         "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+        //"prettier/prettier": ["error", { endOfLine: "auto" }],
     },
 };
